9d362bdcab7153434e8a1e6f2ce3c9ebeae86cb5
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-01-04 19:46:13 -0600

    test katex in component

---
+++ src/app/utils/hooks/useKatex.ts
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react';
+// must import like this - https://github.com/KaTeX/KaTeX/issues/1927#issuecomment-485294236
+import renderMathInElement from 'katex/dist/contrib/auto-render.js';
+
+export const useKatex = () => {
+  // katex render ref
+  const mathRef = useRef(null);
+
+  useEffect(() => {
+    // this could also be applied to the entire App (_app.js) with the Component as dependency
+    // and document.body instead of the ref
+    // https://codesandbox.io/s/elegant-benz-9d9pt?file=/pages/_app.js:35-140
+    if (mathRef.current) {
+      renderMathInElement(mathRef.current, {
+        delimiters: [
+          { left: '$$', right: '$$', display: true },
+          { left: '$', right: '$', display: false },
+          { left: '\\(', right: '\\)', display: false },
+          { left: '\\[', right: '\\]', display: true },
+        ],
+        throwOnError: false,
+      });
+    }
+  }, [mathRef]);
+
+  // dont forget importing the .css in the component
+  // like — import 'katex/dist/katex.min.css';
+  return mathRef;
+};
